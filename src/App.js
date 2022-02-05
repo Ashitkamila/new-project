e27621db98e47f32d70950caaccceb5760e26a50
Chetan <chetanpd51@gmail.com>
2022-02-05 20:38:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Registration from './component/Registration';
+import UpdateEmployee from './component/UpdateEmployee';
 
 function App() {
   return (
     <div className="App">
       <Registration/>
+      <UpdateEmployee />
     </div>
   );
 }
